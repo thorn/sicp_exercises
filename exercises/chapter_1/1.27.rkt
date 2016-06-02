d3fac564ded03955e30d7f74e893f0e0cb6e8ec1
Arsen Shamkhalov <thornu731@gmail.com>
2016-06-02 07:26:30 +0300

    feat: add 1.27 exercise solution

---
+++ exercises/chapter_1/1.27.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |1.27|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(require sicp)
+
+(define (square x) (* x x))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+         m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (begin
+      (cond ((= a 0) 0)
+            ((= (expmod a n n) a) (try-it (- a 1)))
+            (else 1))))
+  (try-it (- n 1)))
+
+(fermat-test 561) ;; => 0
